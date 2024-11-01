a957f424689cbae1635669de83bce378e3b12ee9
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-11-01 17:18:56 -0300

    aula1

--- aulas/aula1.py
+++ aulas/aula1.py
@@ -1,3 +1,25 @@
-print('Olá mundo !')
-for a in range(0,5):
-    print(a)
+
+# print(23)# print(1332)
+
+# a = 10
+# b = 10
+# if a == b:
+#     print('NUMEROS IGUAIS')
+# else:
+#     print('NUMEROS DIFERENTES')
+
+nome = 'Lucas Serrano'
+idade = 18
+maior = idade >= 18
+altura_metros = 1.80
+
+print(f'Nome -> {nome}')
+print(f'Idade -> {idade}')
+print(f'É maior de idade ? {maior}')
+print(f'Qual é sua altura em metros ? {altura_metros}')
+
+nome = 'Lucas Serrano'
+peso = 64
+altura = 180
+imc = (peso/ altura**2) *10000
+print(f'SEU IMC É {imc}')
