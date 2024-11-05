b3689b2ceab578ac02e806cbd4b9c3205aa109cc
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-11-05 14:02:46 -0300

    dia 4

---
+++ aulas/dia4.py
@@ -0,0 +1,93 @@
+# # calculadora com while
+# import time
+
+# def linha_organizada():
+#     print(f'*'*30)
+#     return
+
+# print('BEM VINDO A CALCULADORA !')
+# linha_organizada()
+# while True:
+#     valor_1 = input('DIGITE O PRIMEIRO VALOR ->')
+#     valor_2 = input('DIGITE O SEGUNDO VALOR ->')
+#     operador = input('DIGITE O OPERADOR [+, -, /, *] ->')
+
+#     numero_validos = None
+
+#     try:
+#         valor_1_verificado = float(valor_1)
+#         valor_2_verificado = float(valor_2)
+#         numero_validos = True
+#     except:
+#         numero_validos = None
+
+#     if numero_validos == None:
+#         print('UM OU AMBOS OS NÚMEROS DIGITADOS SÃO INVÁLIDOS')
+#         continue
+
+#     operadores_permitidos = '+-/*'
+
+#     if operador not in operadores_permitidos:
+#         print('Operador inválido')
+#         continue
+
+#     if len(operador) > 1:
+#         print('DIGITE APENAS UM OPERADOR !')
+#         continue
+
+#     if operador == '+':
+#         print('Realizando sua operação...')
+#         time.sleep(2)
+#         linha_organizada()
+#         print(f'{valor_1_verificado} + {valor_2_verificado} = {valor_1_verificado+valor_2_verificado}')
+#         linha_organizada()
+#     elif operador == '-':
+#         print('Realizando sua operação...')
+#         time.sleep(2)
+#         linha_organizada()
+#         print(f'{valor_1_verificado} - {valor_2_verificado} = {valor_1_verificado-valor_2_verificado}')
+#         linha_organizada()
+#     elif operador == '*':
+#         print('Realizando sua operação...')
+#         time.sleep(2)
+#         linha_organizada()        
+#         print(f'{valor_1_verificado} x {valor_2_verificado} = {valor_1_verificado*valor_2_verificado}')
+#         linha_organizada()
+#     elif operador == '/':
+#         print('Realizando sua operação...')
+#         time.sleep(2)
+#         linha_organizada()
+#         print(f'{valor_1_verificado} + {valor_2_verificado} = {valor_1_verificado+valor_2_verificado}')
+#         linha_organizada()
+
+#     sair = input('DESEJA SAIR DO AMBIENTE [SIM/NÃO] ->').lower().strip()
+#     if sair != 'não':
+#         break
+
+# frase = 'O Python é uma linguagem de programação multiparadigma'
+
+# usuario_count = input('QUAL LETRA OU FRASE QUER SABER QUANTAS VEZES PASSA PELA FRASE -->').lower().strip()
+
+# i = 0
+# qtdade = 0
+# while i < len(frase):
+#     letra_atual = frase[i]
+#     if letra_atual == usuario_count:
+#         qtdade += 1
+#     i+=1
+
+# print(f'FOI ENCONTRADO A LETRA {usuario_count}, {qtdade} VEZES')
+
+
+# for i in range(1,6):
+#     for j in range(1,4):
+#         print(f'LINHA{i}, COLUNA{j}')
+
+
+#DESAFIO 1
+# deixe uma palavra secreta
+#a medida que a pessoa colocar uma letra, conte as tentativas
+#deixe astericos e a medida que ela for acertando as letras troque o asteriscos pela letra que forma a palavra
+# quando a pessoa acertar coloque que ela ganhou, a palabvra e quantas tentativas foram necessarias
+# se ela não digitar nada obrigue ela digitar algo
+# se ela digitar algo maior que uma letra, fale que esta inválido
