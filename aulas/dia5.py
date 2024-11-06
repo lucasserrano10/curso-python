f4b81c9b8c63500fcaadbe6f0e5e80a02ec305df
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-11-06 10:28:35 -0300

    dia5

---
+++ aulas/dia5.py
@@ -0,0 +1,33 @@
+# nomes = ['Lucas Serrano', 'Bárbara Barbetta', 'Arthur Lira']
+# #pop()
+
+# nomes.pop()
+# print(nomes)
+
+# #APAGA O ÚLTIMO ELEMENTO DA LISTA
+
+# nomes.append('Arthur Lira')
+# print(nomes)
+
+# # ADICIONA ALGO NA LISTA
+
+# del nomes[1]
+# print(nomes)
+
+# # apaga qualquer elemento da lista a partir de um indice
+
+# # nomes.clear()
+# # print(nomes)
+
+# # nomes.append('Lucas Serrano')
+# # nomes.append('Arthur Lira')
+# # nomes.append('Bárbara Barbetta')
+# # print(nomes)
+
+# nomes.insert(0,'OLA')
+
+lista_a = [1,2,3,4]
+lista_b = [5,6,7,8,9]
+lista_a.extend(lista_b)
+
+print(lista_a)
